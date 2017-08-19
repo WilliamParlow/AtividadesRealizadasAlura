3d38690c5ee0e3505f9b1e5f8672e5ac80e3d3e4
WilliamParlow <william.nichelatti@catolicasc.org.br>
2017-08-19 01:16:10 -0300

    Advanced CSS II finished

--- JavaScriptAvançado/Negociacoes/client/js/app/services/HttpService.js
+++ JavaScriptAvançado/Negociacoes/client/js/app/services/HttpService.js
@@ -15,7 +15,7 @@ class HttpService {
         if (xhr.readyState == 4) {
 
           if (xhr.status == 200) {
-
+            console.log(xhr)
             resolve(JSON.parse(xhr.responseText));
           } else {
             reject('Não foi possível obter dados da semana anterior');
