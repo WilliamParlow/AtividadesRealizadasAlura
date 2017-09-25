b56a4cd2c7b2c8c7afdbc16f47b990419f868562
WilliamParlow <william.nichelatti@catolicasc.org.br>
2017-09-25 12:00:34 -0300

    update on my bar chart

--- LogicaDeProgII/myGraphTest/myApp.js
+++ LogicaDeProgII/myGraphTest/myApp.js
@@ -1,8 +1,12 @@
 function appStart() {
 
-   chartConf = getConfig();
+   if (!chartConfig) {
+      chartConfig = getConfig();
+   }
 
-   drawGraph(chartConf);
+   graphBarMap = {};
+
+   drawGraph(chartConfig);
 
 }
 
@@ -17,64 +21,21 @@ function drawGraph(chartConf) {
 
          let value = chartConf.values[i][j];
 
-         let y = map(value, 0, highVal, startY, endY);
-         let size = endX / 12;
-         let height = endY - 15;
+         let y = (endY - map(value, 0, highVal, startY, endY)) + LIMIT_Y;
+         let size = Math.round(endX / totalValues * 0.95);
+         let height = endY;
 
-         drawHelpers();
-         drawBar(x, y, size, height);
-         drawText(x, y, `${value}`, "12px Arial");
+         drawHelpers(mainContext);
+         drawBar(x, y, size, height, undefined, mainContext);
+         drawStroke(x, y, size, height, undefined, mainContext);
+         mapObject(x, y, size, height, value, mainContext);
+         drawText(x, y, `${value.toLocaleString('pt-br')}`, size, "bold 12px Arial", undefined, true, mainContext);
 
          x += size;
 
       }
 
    }
-
-}
-
-function getHighVal(vals) {
-
-   let highVals = [];
-
-   for (let i = 0; i < vals.length; i++) {
-      highVals.push(Math.max(...vals[i]));
-   }
-
-   return Math.max(...highVals);
-
-}
-
-function drawBar(x, y, width, height) {
-
-   context.fillStyle = "red";
-   context.fillRect(x, y, width, height);
-
-}
-
-function drawText(x, y, text, fontConfig, color) {
-
-   fontConfig = fontConfig || "Arial 12px";
-   color = color || "black";
-
-   context.font = fontConfig;
-   context.fillStyle = color;
-   context.fillText(text, x, y);
-
-}
-
-function drawHelpers() {
-
-   context.fillStyle = 'black';
-
-   context.fillRect(startX - 5, startY + 5, 5, endY);
-   context.fillRect(startX - 5, window.innerHeight - 4, endX, 5);
-
-}
-
-function map(x, in_min, in_max, out_min, out_max) {
-  
-   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 
 }
 
